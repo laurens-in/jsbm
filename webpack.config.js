e2857d0132e751203acbbda4eab777e1a4065392
Cedric Spindler <cedric.spindler@gmail.com>
2020-04-08 01:21:13 +0200

    First attempt to get ready for webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    path: '/',
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+      { test: /\.tsx?$/, use: ["ts-loader"], exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "./src/index.html",
+		})
+	],
+  devtool: 'inline-source-map'
+};
