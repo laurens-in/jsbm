5f4566a71e727f4dbc7c5202822c7379efe5ede4
Cedric Spindler <cedric.spindler@gmail.com>
2020-04-08 02:17:29 +0200

    Implement demo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
 // Entrypoint
+import { Frequency, Time, Synth, context } from 'tone/Tone';
+
+context.rawContext.suspend
+
+document.addEventListener('DOMContentLoaded', () => {
+  // resume playback to enable Audio API in all browsers
+  document.querySelector('button.play')?.addEventListener('click', function() {
+    context.resume().then(() => {
+        console.log('Playback resumed successfully');
+    });
+  });
+  document.querySelector('button.stop')?.addEventListener('click', function() {
+
+    context.rawContext.suspend().then(() => {
+      console.log('Playback suspended successfully...');
+    });
+  });
+});
+
+const synth = new Synth();
+
+synth.toMaster();
+
+synth.triggerAttackRelease(
+  new Frequency('C#3'),
+  new Time('8n')
+);
