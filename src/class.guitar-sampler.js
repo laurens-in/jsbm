a4c8ba95b4154769cfc9bcb00a3de39016e14947
laurens-in <laurens.hwm@gmail.com>
2020-06-15 12:22:00 +0200

    more saaaampleeeezzzz

--- src/class.guitar-sampler.js
+++ src/class.guitar-sampler.js
@@ -25,48 +25,64 @@ class GuitarSampler {
 
         this.guitar1 = new Tone.Sampler({
             'E2' : "Guitar/Guitar_ESP_E2_-_1.opus",
+            'E3' : "Guitar/Guitar_ESP_E3_-_1.opus",
+            'E4' : "Guitar/Guitar_ESP_E4_-_1.opus"
         }, () => {
             this.guitar1.connect(this.guitarOut);
         }, baseurl);
 
         this.guitar2 = new Tone.Sampler({
             'E2' : "Guitar/Guitar_ESP_E2_-_2.opus",
+            'E3' : "Guitar/Guitar_ESP_E3_-_2.opus",
+            'E4' : "Guitar/Guitar_ESP_E4_-_2.opus"
         }, () => {
             this.guitar2.connect(this.guitarOut);
         }, baseurl);
 
         this.guitar3 = new Tone.Sampler({
             'E2' : "Guitar/Guitar_ESP_E2_-_3.opus",
+            'E3' : "Guitar/Guitar_ESP_E3_-_3.opus",
+            'E4' : "Guitar/Guitar_ESP_E4_-_3.opus"
         }, () => {
             this.guitar3.connect(this.guitarOut);
         }, baseurl);
 
         this.guitar4 = new Tone.Sampler({
             'E2' : "Guitar/Guitar_ESP_E2_-_4.opus",
+            'E3' : "Guitar/Guitar_ESP_E3_-_4.opus",
+            'E4' : "Guitar/Guitar_ESP_E4_-_4.opus"
         }, () => {
             this.guitar4.connect(this.guitarOut);
         }, baseurl);
 
         this.guitar5 = new Tone.Sampler({
             'E2' : "Guitar/Guitar_ESP_E2_-_5.opus",
+            'E3' : "Guitar/Guitar_ESP_E3_-_5.opus",
+            'E4' : "Guitar/Guitar_ESP_E4_-_5.opus"
         }, () => {
             this.guitar5.connect(this.guitarOut);
         }, baseurl);
 
         this.guitar6 = new Tone.Sampler({
             'E2' : "Guitar/Guitar_ESP_E2_-_6.opus",
+            'E3' : "Guitar/Guitar_ESP_E3_-_6.opus",
+            'E4' : "Guitar/Guitar_ESP_E4_-_6.opus"
         }, () => {
             this.guitar6.connect(this.guitarOut);
         }, baseurl);
 
         this.guitar7 = new Tone.Sampler({
             'E2' : "Guitar/Guitar_ESP_E2_-_7.opus",
+            'E3' : "Guitar/Guitar_ESP_E3_-_7.opus",
+            'E4' : "Guitar/Guitar_ESP_E4_-_7.opus"
         }, () => {
             this.guitar7.connect(this.guitarOut);
         }, baseurl);
 
         this.guitar8 = new Tone.Sampler({
             'E2' : "Guitar/Guitar_ESP_E2_-_8.opus",
+            'E3' : "Guitar/Guitar_ESP_E3_-_8.opus",
+            'E4' : "Guitar/Guitar_ESP_E4_-_8.opus"
         }, () => {
             this.guitar8.connect(this.guitarOut);
         }, baseurl);
